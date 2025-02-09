25c0b55c7c9e31094b7a3876d46e0b2d816b472c
Krampuso <al849783@ucf.edu>
2025-02-09 16:03:35 -0500

    Solution 1 Done

--- Lab 5/Assets/scripts/SolutionOne.cs
+++ Lab 5/Assets/scripts/SolutionOne.cs
@@ -6,7 +6,6 @@ using UnityEngine.UIElements;
 public class SolutionOne : MonoBehaviour
 {
     public string characterName;
-    public string selectedClass;
     public int characterLevel;
     public int characterCon;
     private float characterHP;
@@ -21,25 +20,24 @@ public class SolutionOne : MonoBehaviour
 
     };
 
-    Dictionary<string, int> dndDiction = new Dictionary<string, int>
+    Dictionary<string, int> hitDice = new Dictionary<string, int>
     {
         { "Artificer",8}, {"Barbarian",12 }, {"Bard",8 }, {"Cleric",8 }, {"Druid",8 }, {"Fighter",10 },{ "Monk",8 },
         { "Ranger",10 }, {"Rogue",8 }, {"Paladin",10 }, {"Sorcerer",6 }, {"Wizard",6 }, {"Warlock",8}
     };
 
-    public int[] modifiers = new int[]
+    private int[] modifiers = new int[]
     {
         -5,-4,-4,-3,-3,-2,-2,-1,-1,0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10
     };
+
+    public string selectedClass = "";
     
-
-
-
 
     // Start is called before the first frame update
     void Start()
     {
-      Debug.Log("My character " + characterName + "is a level " + characterLevel);
+      GetHP();
     }
 
     // Update is called once per frame
@@ -50,13 +48,18 @@ public class SolutionOne : MonoBehaviour
 
     public void rightclass()
     {
-        if(selectedClass == characterClasses)
+        if(characterClasses.Contains(selectedClass))
         {
-            selectedClass = characterClasses;
+          Debug.Log("Valid class");
         }
-        else
+        else if(!characterClasses.Contains(selectedClass))
         {
-            Debug.Log("Invalid Class");
+            Debug.Log("Invalid class restart code");
+            foreach(string characterClass in characterClasses)
+            {
+                Debug.Log(characterClass);
+            }
+            return;  
         }
     }
 
@@ -66,16 +69,37 @@ public class SolutionOne : MonoBehaviour
         characterLevel = Mathf.Clamp(characterLevel,1,30);
     }
 
-    private void GetHP(Dictionary<string, int> die, string class1, int die2) //Look over
+    private void GetHP() //Look over
     {
+        rightclass();
+        int hitDie = hitDice[selectedClass];
+        int conModifier = modifiers[characterCon - 1];
+
+         characterHP = hitDie + conModifier; //Level 1 hp
+        
         if(averageHP)
         {
-            characterHP = ((die2 * characterLevel) / 2f) + 0.5f;
+            float avgRoll = (hitDie / 2.0f) + 0.5f;
+            characterHP += Mathf.FloorToInt((characterLevel - 1) * (avgRoll + conModifier));
         }
-        //else if(averageHP == false)
-       // {
-            //rolled hp
-        //}
-       
+        else
+        {
+            for(int i =1; i < characterLevel; i++)
+            {
+                characterHP += Random.Range(1, hitDie + 1) + conModifier;
+            }   
+        }
+        if(isHillDwarf)
+        {
+            characterHP += characterLevel;
+        }
+
+        if(toughFeat)
+        {
+            characterHP += characterLevel * 2;
+        }
+
+        Debug.Log($"My character {characterName} is a level {characterLevel} {selectedClass} with a CON score of {characterCon} and {(isHillDwarf ? "is" : "is not")} a Hill Dwarf and {(toughFeat ? "has" : "does not have")} the Tough feat. I want the HP { (averageHP ? "averaged" : "rolled") }.");
+
     }
 }
